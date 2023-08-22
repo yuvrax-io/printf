171f495c6e027a6bb971248dead6eaa861e32817
ImraneBendarham <Bendhimran@gmail.com>
2023-08-22 02:32:18 -0700

    main.h

--- main.h
+++ main.h
@@ -6,11 +6,17 @@
 #include <stdio.h>
 
 #define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
 
+#define F_HASH 8
 
 int print_binary(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_hexadecimal(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_hexa(va_list types, char map_to[],
+char buffer[], int flags, char flag_ch, int width, int precision, int size);
+int print_hexa_upper(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
 #endif /* MAIN_H */
