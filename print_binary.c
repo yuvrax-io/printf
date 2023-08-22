1b504601823d62b12278ec46ea3b2f4cc01dc663
ImraneBendarham <Bendhimran@gmail.com>
2023-08-22 01:19:20 -0700

    fix binarie

--- print_binary.c
+++ print_binary.c
@@ -31,7 +31,16 @@ int print_binary(va_list types, char buffer[],
 		m /= 2;
 		a[i] = (n / m) % 2;
 
-		count++;
 	}
+	for(i = 0, sum = 0, count = 0; i < 32; i++)
+	{
+		sum += a[i];
+		if(sum || i == 31)
+		{
+			char z = '0' + a[i];
+
+			write(1, &z, 1);
+		}
+
 	return (count);
 }
