e86e0c318b19c77abb5e2521fa5fa1190fa2f219
ImraneBendarham <Bendhimran@gmail.com>
2023-08-22 01:33:01 -0700

    main

--- main.h
+++ main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#define UNUSED(x) (void)(x)
+
+
 int print_binary(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
