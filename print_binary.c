c3d0cd6bcebd14e1ee0f2be5a992f7f42ab749f6
ImraneBendarham <Bendhimran@gmail.com>
2023-08-22 01:24:47 -0700

    betty style

--- print_binary.c
+++ print_binary.c
@@ -32,7 +32,7 @@ int print_binary(va_list types, char buffer[],
 		a[i] = (n / m) % 2;
 
 	}
-	for(i = 0, sum = 0, count = 0; i < 32; i++)
+	for (i = 0, sum = 0, count = 0; i < 32; i++)
 	{
 		sum += a[i];
 		if (sum || i == 31)
