fdc1f157bd2bae01fd43425c925db7f71365aa73
yousri rachid <yuvrax.io@gmail.com>
2023-08-22 05:35:45 -0700

    modify

--- main.h
+++ main.h
@@ -29,7 +29,9 @@ char buffer[],
 	int flags, int width, int precision, int size);
 int write_number(int is_positive, int ind, char buffer[],
 	int flags, int width, int precision, int size);
-
+int is_printable(char);
+int append_hexa_code(char, char[], int);
+int is_digit(char);
 
 long int convert_size_unsgnd(unsigned long int num, int size);
 long int convert_size_number(long int num, int size);
